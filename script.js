6ed4089c6231304daa40a2500f0458604646cb57
Harrison Metz <harrisonmetz17@gmail.com>
2025-07-16 14:38:12 -0800

    Added user interface elements and basic functions

---
+++ script.js
@@ -0,0 +1,25 @@
+let num1;
+let operator;
+let num2;
+
+
+
+function add(){
+
+};
+
+function subtract(){
+
+};
+
+function multiply(){
+
+};
+
+function divide(){
+
+};
+
+function operate(a, b, op){
+
+};
