3c5c8e609f057f86f6e0912e52175213cdfb7efa
Harrison Metz <harrisonmetz17@gmail.com>
2025-07-18 11:51:33 -0800

    Added flex attributes and number button function

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 let num1;
 let operator;
 let num2;
+let equation = '';
 
+const inputBox = document.getElementById('inputBox');
+const equalsBtn = document.getElementById('equals');
+const numberButtons = document.querySelectorAll('.num');
 
+numberButtons.forEach(button => {
+    button.addEventListener('click', function(){
+        equation += button.innerHTML;
+        inputBox.value = equation;
+    })
+});
 
+equalsBtn.addEventListener('click', function(){
+    console.log(equation);
+})
 function add(){
 
 };
@@ -25,7 +38,9 @@ function mod(){
 };
 
 function clear(){
-
+    num1 = '';
+    operator = '';
+    num2 = '';
 };
 
 function operate(a, b, op){
