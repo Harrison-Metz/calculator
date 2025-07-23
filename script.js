f846680d8737b6ba029124bff669d508761b803f
Harrison Metz <harrisonmetz17@gmail.com>
2025-07-22 20:30:27 -0800

    revised previous function and implemented number buttons and some operators.

--- script.js
+++ script.js
@@ -1,48 +1,67 @@
-let num1;
-let operator;
-let num2;
-let equation = '';
+let num1 = '';
+let operator = '';
+let num2 = '';
+let input = '';
+let opCounter = 0;
+let answer = '';
 
 const inputBox = document.getElementById('inputBox');
 const equalsBtn = document.getElementById('equals');
 const numberButtons = document.querySelectorAll('.num');
+const operatorButtons = document.querySelectorAll('.op');
+const clearBtn = document.getElementById('clear');
+
+function operate(a, b, op){
+    let A = parseInt(a);
+    let B = parseInt(b);
+    if(op === '+'){
+        return A + B;
+    }else if(op === '-'){
+        return A - B;
+    }
+};
 
 numberButtons.forEach(button => {
     button.addEventListener('click', function(){
-        equation += button.innerHTML;
-        inputBox.value = equation;
-    })
+        input += button.innerHTML;
+        inputBox.value = input;
+    });
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', function(){
+        if(opCounter === 0){
+            num1 = input;
+            operator = button.innerHTML;
+            opCounter++;
+            input = '';
+            inputBox.value = input;
+        }else{
+            num2 = inputBox.value;
+            let temp = operate(num1, num2, operator);
+            num1 = temp;
+            operator = button.innerHTML;
+            inputBox.value = num1
+        }
+        
+    });
 });
 
 equalsBtn.addEventListener('click', function(){
-    console.log(equation);
-})
-function add(){
+    if(opCounter === 0){
+        num2 = inputBox.value;
+        answer = operate(num1, num2, operator);
+    }
+    inputBox.value = answer;
+});
 
-};
-
-function subtract(){
-
-};
-
-function multiply(){
-
-};
-
-function divide(){
-
-};
-
-function mod(){
-
-};
-
-function clear(){
+clearBtn.addEventListener('click', function(){
     num1 = '';
-    operator = '';
     num2 = '';
-};
+    operator = '';
+    input = '';
+    answer = '';
+    opCounter = 0;
+});
 
-function operate(a, b, op){
-
-};
+console.log(operate('5', '9', '+'));
