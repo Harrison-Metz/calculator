6c0e58bcd308759aab9d2a5f20d90e3c5ee83002
Harrison Metz <harrisonmetz17@gmail.com>
2025-07-23 16:30:29 -0800

    Full functionality for all operators, clear and equals buttons

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let num2 = '';
 let input = '';
 let opCounter = 0;
 let answer = '';
+let opClicked = false;
 
 const inputBox = document.getElementById('inputBox');
 const equalsBtn = document.getElementById('equals');
@@ -14,15 +15,27 @@ const clearBtn = document.getElementById('clear');
 function operate(a, b, op){
     let A = parseInt(a);
     let B = parseInt(b);
+    // operator = '';
     if(op === '+'){
         return A + B;
     }else if(op === '-'){
         return A - B;
+    }else if(op === 'x'){
+        return A * B;
+    }else if(op === '/'){
+        return A / B;
+    }else if(op === '%'){
+        return A % B;
     }
 };
 
 numberButtons.forEach(button => {
     button.addEventListener('click', function(){
+        if(opCounter > 0){
+            input = '';
+            num2 = '';
+        }
+        opClicked = false;
         input += button.innerHTML;
         inputBox.value = input;
     });
@@ -30,29 +43,41 @@ numberButtons.forEach(button => {
 
 operatorButtons.forEach(button => {
     button.addEventListener('click', function(){
-        if(opCounter === 0){
-            num1 = input;
-            operator = button.innerHTML;
-            opCounter++;
-            input = '';
-            inputBox.value = input;
+        if(!opClicked){
+            if(opCounter === 0){
+                opClicked = true;
+                num1 = input;
+                operator = button.innerHTML;
+                opCounter++;
+                input = '';
+                inputBox.value = input;
+            }else{
+                opClicked = true;
+                num2 = inputBox.value;
+                let temp = operate(num1, num2, operator);
+                num1 = temp;
+                operator = button.innerHTML;
+                inputBox.value = num1
+            }
         }else{
-            num2 = inputBox.value;
-            let temp = operate(num1, num2, operator);
-            num1 = temp;
-            operator = button.innerHTML;
-            inputBox.value = num1
+            console.log('operator: ' + operator);
+            console.log('button: ' + button.innerHTML);
+            if(operator != button.innerHTML){
+                operator = button.innerHTML;
+            }
         }
-        
     });
 });
 
 equalsBtn.addEventListener('click', function(){
-    if(opCounter === 0){
+    if(opCounter === 1){
         num2 = inputBox.value;
         answer = operate(num1, num2, operator);
+        inputBox.value = answer;
+        input = inputBox.value;
+        opCounter = 0;
+        console.log(answer);
     }
-    inputBox.value = answer;
 });
 
 clearBtn.addEventListener('click', function(){
@@ -62,6 +87,5 @@ clearBtn.addEventListener('click', function(){
     input = '';
     answer = '';
     opCounter = 0;
+    inputBox.value = '';
 });
-
-console.log(operate('5', '9', '+'));
